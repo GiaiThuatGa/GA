f3400263e31f9e2a1b9ac1aef9338aab8e079c7f
thiensuxlawst <thiensuxlawst@gmail.com>
2017-04-28 10:40:50 +0700

    Đọc ghi file text, tạo cấu trúc của trạm(Tram) và khoảng cách trạm(Pair)

---
+++ GA/Form1.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace GA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnReadText = new System.Windows.Forms.Button();
+            this.btnWriteText = new System.Windows.Forms.Button();
+            this.btnTinhKC = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnReadText
+            // 
+            this.btnReadText.Location = new System.Drawing.Point(58, 30);
+            this.btnReadText.Name = "btnReadText";
+            this.btnReadText.Size = new System.Drawing.Size(75, 42);
+            this.btnReadText.TabIndex = 0;
+            this.btnReadText.Text = "Đọc Text";
+            this.btnReadText.UseVisualStyleBackColor = true;
+            this.btnReadText.Click += new System.EventHandler(this.btnReadText_Click);
+            // 
+            // btnWriteText
+            // 
+            this.btnWriteText.Location = new System.Drawing.Point(195, 30);
+            this.btnWriteText.Name = "btnWriteText";
+            this.btnWriteText.Size = new System.Drawing.Size(75, 42);
+            this.btnWriteText.TabIndex = 0;
+            this.btnWriteText.Text = "Ghi Text";
+            this.btnWriteText.UseVisualStyleBackColor = true;
+            this.btnWriteText.Click += new System.EventHandler(this.btnWriteText_Click);
+            // 
+            // btnTinhKC
+            // 
+            this.btnTinhKC.Location = new System.Drawing.Point(58, 130);
+            this.btnTinhKC.Name = "btnTinhKC";
+            this.btnTinhKC.Size = new System.Drawing.Size(102, 42);
+            this.btnTinhKC.TabIndex = 0;
+            this.btnTinhKC.Text = "Tính khoảng cách";
+            this.btnTinhKC.UseVisualStyleBackColor = true;
+            this.btnTinhKC.Click += new System.EventHandler(this.btnTinhKC_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 281);
+            this.Controls.Add(this.btnTinhKC);
+            this.Controls.Add(this.btnWriteText);
+            this.Controls.Add(this.btnReadText);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnReadText;
+        private System.Windows.Forms.Button btnWriteText;
+        private System.Windows.Forms.Button btnTinhKC;
+    }
+}
+
---
+++ GA/Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public struct Tram
+        {
+         public   String MaTram;
+         public   Double Longitude;
+         public   Double Latitude;
+         public  Double DoCao;
+        }
+
+        struct Pair
+        {
+            public String MaTram1;
+            public String MaTram2;
+            public Double KhoangCach;
+        }
+
+        
+        List<Tram> trams = new List<Tram>();
+        List<Pair> D = new List<Pair>();
+       
+        private void btnReadText_Click(object sender, EventArgs e)
+        {
+            String[] lines = System.IO.File.ReadAllLines(Application.StartupPath +@"\Inp.txt");
+            
+            for(int i=0;i<lines.Count(); i++)
+            {
+                String[] temp = lines[i].Split(' ');
+                Tram temp1 = new Tram();
+                temp1.MaTram = temp[0];
+                temp1.Longitude = Double.Parse(temp[1]);
+                temp1.Latitude = Double.Parse(temp[2]);
+                temp1.DoCao = Double.Parse(temp[3]);
+                trams.Add(temp1);
+            }
+
+            MessageBox.Show("Đọc thành công");
+
+         
+
+        
+        }
+
+        private void btnWriteText_Click(object sender, EventArgs e)
+        {
+            System.IO.StreamWriter file = new System.IO.StreamWriter(Application.StartupPath + @"\Outp.txt");
+            for (int i = 0; i < trams.Count; i++)
+            {
+                String temp = trams[i].MaTram + " " + trams[i].Longitude + " " + trams[i].Latitude + " " + trams[i].DoCao;
+                file.WriteLine(temp);
+            }
+            file.Close();
+            MessageBox.Show("Ghi thành công");
+        }
+
+        private void btnTinhKC_Click(object sender, EventArgs e)
+        {
+            //int n = trams.Count;
+            //for (int i = 0; i < n; i++)
+            //    for (int j = 0; j < n; j++)
+            //    {
+
+            //    }
+        }
+    }
+}
